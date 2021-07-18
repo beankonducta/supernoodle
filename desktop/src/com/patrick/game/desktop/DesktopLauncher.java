cd7c41b04fce34b2b01717c4b2e73fb82452aaeb
patrick andrews <patrickxandrews@gmail.com>
2021-07-18 15:34:31 -0600

    change build.gradle to hopefully work on rpi

--- desktop/src/com/patrick/game/desktop/DesktopLauncher.java
+++ desktop/src/com/patrick/game/desktop/DesktopLauncher.java
@@ -1,12 +1,16 @@
 package com.patrick.game.desktop;
 
+import com.badlogic.gdx.backends.jogamp.JoglNewtApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.jogamp.JoglNewtApplicationConfiguration;
 import com.patrick.game.SuperNoodle;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        new LwjglApplication(new SuperNoodle(), config);
+//        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+//        new LwjglApplication(new SuperNoodle(), config);
+        JoglNewtApplicationConfiguration config = new JoglNewtApplicationConfiguration();
+        new JoglNewtApplication(new SuperNoodle(), config);
     }
 }
